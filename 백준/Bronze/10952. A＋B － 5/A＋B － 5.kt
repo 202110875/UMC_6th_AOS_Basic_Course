f8112da55c3596efc6fb9441ebaf0c4b7a12970a
SeoHyeonJeong <95207365+202110875@users.noreply.github.com>
2024-04-15 16:26:22 +0900

    [Bronze V] Title: A+B - 5, Time: 144 ms, Memory: 13756 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val s=Scanner(System.`in`)
+    while(true){
+        var a=s.nextInt()
+        var b=s.nextInt()
+        if(a==0 && b==0) break;
+        println(a+b)
+    }
+}
